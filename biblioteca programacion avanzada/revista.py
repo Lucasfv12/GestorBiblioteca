c481ecf93652cfde511d7519a8427b216044593e
Carl-2401 <brizuelac2401@gmail.com>
2026-06-22 23:46:59 -0300

    Delete biblioteca programacion avanzada directory
    Error al subir, ya lo soluciono

--- biblioteca programacion avanzada/revista.py
+++
@@ -1,9 +0,0 @@
-from material import Material
-
-class Revista (Material):
-        def __init__(self,codigo,titulo,numero_edicion):
-            super().__init__(codigo,titulo)
-            self.__numero_edicion = numero_edicion
-    
-        def dias_de_prestamo(self):
-            return 7
